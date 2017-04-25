994ff907986bc5a998b551dd36bd99c04df4e2c4
Rian Quinn <rianquinn@gmail.com>
2017-04-25 09:11:29 -0600

    build files removed
    Signed-off-by: Rian Quinn <“rianquinn@gmail.com”>

--- src/arch/linux/.bareflank.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/user/bfdriver/src/arch/linux/bareflank.o := ld -m elf_x86_64   -r -o /home/user/bfdriver/src/arch/linux/bareflank.o /home/user/bfdriver/src/arch/linux/entry.o /home/user/bfdriver/src/arch/linux/vmcall.o /home/user/bfdriver/src/arch/linux/platform.o /home/user/bfdriver/src/arch/linux/../../common.o 
--- src/arch/linux/.vmcall.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/user/bfdriver/src/arch/linux/vmcall.o := nasm -f elf64 -o vmcall.asm
